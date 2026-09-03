502f0d9c47fab8448404f48adccfa393b0b575ca
TanuMourya <tanumourya19@gmail.com>
2026-09-03 17:50:35 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (75.77%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,7 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         //MOORE'S algorithm
         int freq=0;
-        int ans=nums[0];
+        int ans=0;
         for(int i=0;i<nums.size();i++){
             if(freq==0)ans=nums[i];
             if(ans==nums[i])freq++;
